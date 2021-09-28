cdcec56bc559ec51e0951cf617b481fd57238736
Marcel <marcel.endres@student.htw-berlin.de>
2021-09-28 17:28:15 +0000

    db connection and basic get/set

--- database.js
+++ database.js
@@ -1,3 +1,29 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://217.79.181.125:27017');
 
+// db schemas
+const EventSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+// init models
+const Event = mongoose.model('Event', EventSchema);
+
+// db functions
+async function dbConnect() {
+  await mongoose.connect('mongodb://217.79.181.125:27017');
+}
+
+async function addEvent(pName) {
+  const newEvent = new Event({ name: pName, description: 'Hello there' });
+  const newId = newEvent.id;
+  await newEvent.save();
+  return newId;
+}
+
+async function getEvents() {
+  console.log(await Event.find());
+}
+
+// export
+module.exports = { dbConnect, addEvent, getEvents };
